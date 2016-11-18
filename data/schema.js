0f8790064f20293a9fcf51b8c353f772d17b67ad
Mauricio Ribeiro <maumribeiro@gmail.com>
2016-11-18 17:50:21 +0100

    Adding Mutations

--- data/schema.js
+++ data/schema.js
@@ -1,53 +1,10 @@
-import { GraphQLSchema, GraphQLObjectType, GraphQLList, GraphQLString, GraphQLInt, GraphQLFloat } from 'graphql';
-import Drink from './model/Drink';
+import { GraphQLSchema } from 'graphql';
+import QueryType from './types/QueryType';
+import MutationType from './types/MutationType';
 
-let Schema = (db) => {
-  // declaration of our type Drink
-  let drinkType = new GraphQLObjectType({
-      name: "Drink",
-      fields: () => ({ // description of the fields { name_of_the_field: type_of_the_field }
-          _id: { type: GraphQLString },
-          name: { type: GraphQLString },
-          type1: { type: GraphQLString },
-          type2: { type: GraphQLString },
-          price: { type: GraphQLFloat },
-          stock: { type: GraphQLInt }
-      })
-  });
-
-  // the schema of the GraphQL queries
-  let schema = new GraphQLSchema({
-    query: new GraphQLObjectType({
-        name:'Query',
-        fields: () => ({
-            drinks: {
-                type: new GraphQLList(drinkType),
-                args: { // for filtering
-                    name: {
-                        name: 'name',
-                        type: GraphQLString
-                    }
-                },
-                resolve: (root, {name}) => 
-                { 
-                    if (name != null && name != "")
-                        return Drink.find({'name': name}).exec();
-                    
-                    return Drink.find().exec(function(err, drinksArr){
-                        if(err) console.log(err);
-                        else console.log('no errors');
-                        console.log(drinksArr);
-                        return drinksArr;
-                    });
-                }
-                // function that GraphQL will execute in order to resolve the field
-                // here we get all the candidates from the DB
-            }
-        })
-    })
-  });
-
-  return schema
-};
+let Schema = new GraphQLSchema({
+    query: QueryType,
+    mutation: MutationType
+});
 
 export default Schema;
