34e68f78de346f401bad1dfc6f6c83a96ca058f9
Mauricio Ribeiro <Mauricio Ribeiro>
2016-11-21 21:27:01 +0100

    fixing issues with schema

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import schema from './data/schema';
+import Schema from './data/schema';
 import GraphQLHTTP from 'express-graphql';
 import mongoose from 'mongoose';
 
@@ -11,15 +11,14 @@ mongoose.connect('mongodb://localhost:27017/dbdrinks');
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log('DB connected');
+    console.log('DB connected');
 });
 
 app.use('/graphql', GraphQLHTTP({
-    schema: schema(db),
-    graphiql: true   // to use GraphiQL :)
+    schema: Schema,
+    graphiql: true // to use GraphiQL :)
 }));
 
-app.listen(3000, function(){
-  console.log('Server running in port 3000');
+app.listen(3000, function() {
+    console.log('Server running in port 3000');
 });
-
